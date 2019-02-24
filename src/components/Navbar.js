e09c9701d920f24af595849ccc8d910bd3e9879b
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-02-24 14:51:03 -0500

    Stylistic updates to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -44,7 +43,6 @@ const Navbar = class extends React.Component {
       </div>
     </div>
     <div id="navMenu" className="navbar-menu">
-    <div className="navbar-start has-text-centered">
       <Link className="navbar-item" to="/work">
         Work
       </Link>
@@ -60,19 +58,6 @@ const Navbar = class extends React.Component {
       <Link className="navbar-item" to="/contact/examples">
         Form Examples
       </Link>
-    </div>
-    <div className="navbar-end has-text-centered">
-      <a
-        className="navbar-item"
-        href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <span className="icon">
-          <img src={github} alt="Github" />
-        </span>
-      </a>
-    </div>
     </div>
   </nav>
   )}
