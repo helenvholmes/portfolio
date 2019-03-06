b4d25b96308c841398a9c4d010cc4a675d4c44eb
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-03-06 16:05:38 -0500

    This is a little further along

--- src/components/FeaturedWork.js
+++ src/components/FeaturedWork.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import FeaturedWorkStyles from "./FeaturedWork.module.css"
 
 const FeaturedWork = class extends React.Component {
@@ -6,7 +7,14 @@ const FeaturedWork = class extends React.Component {
   render() {
     return (
       <section className={FeaturedWorkStyles.section}>
-        <p>The featured work section</p>
+        <h2>Featured Work</h2>
+
+        <div className={FeaturedWorkStyles.work}>
+          <Link className={FeaturedWorkStyles.link} to="/writing/the-things-we-own">
+            Welcome to Aspen. <br />
+            We're glad you're here.
+          </Link>
+        </div>
       </section>
     )
   }
