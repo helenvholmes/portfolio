67d0a25822e6c136500a3c1242f269146beda0dd
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-03-07 11:50:49 -0500

    Idk

--- src/components/FeaturedLogos.js
+++ src/components/FeaturedLogos.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeaturedLogosStyles from "./FeaturedLogos.module.css"
 import HeadersStyles from "./Headers.module.css"
+import logo from '../img/logo.svg'
 
 const FeaturedLogos = class extends React.Component {
 
@@ -10,6 +11,14 @@ const FeaturedLogos = class extends React.Component {
         <h2 className={[HeadersStyles.featured, HeadersStyles.rotate].join(' ')}>Logos</h2>
         <h2 className={HeadersStyles.featured}>+</h2>
         <h2 className={HeadersStyles.featured}>Additional Projects</h2>
+
+        <div className={FeaturedLogosStyles.logosGrid}>
+        <img src={logo} alt="Helen V. Holmes" style={{ width: '60px' }} />
+        <img src={logo} alt="Helen V. Holmes" style={{ width: '60px' }} />
+        <img src={logo} alt="Helen V. Holmes" style={{ width: '60px' }} />
+        <img src={logo} alt="Helen V. Holmes" style={{ width: '60px' }} />
+        <img src={logo} alt="Helen V. Holmes" style={{ width: '60px' }} />
+        </div>
       </section>
     )
   }
