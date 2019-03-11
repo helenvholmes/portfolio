095788b2c1184fb27e3d7dcd046342fcfea9da86
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-03-11 15:49:29 -0400

    Ugh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,28 +6,27 @@ import NavbarStyles from "./Navbar.module.css"
 const Navbar = class extends React.Component {
 
   componentDidMount() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbarBurger'), 0);
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
- 
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
- 
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
- 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-        });
-      });
+    console.log('I was triggered during componentDidMount')
+    // Get the "#navbarMenu" element
+    const $navbarMenu = document.querySelector('#navbarMenu');
+
+    // Add a click event to menu
+    $navbarMenu.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = $navbarMenu.dataset.target;
+
+      // Toggle the "is-active" class on "#navbarMenu"
+      $navbarMenu.classList.toggle('is-active');
+    });
+
+    if (($navbarMenu.classList.contains('is-active')) === "true") {
+      console.log('ooo-gah boo-gah!');
     }
   }
 
   render() {
+    console.log('I was triggered during render')
     return (
       <nav className={NavbarStyles.navbar} role="navigation" aria-label="main-navigation">
         <div className={NavbarStyles.navbarBrand}>
@@ -35,11 +34,13 @@ const Navbar = class extends React.Component {
             <img src={logo} alt="Helen V. Holmes" style={{ width: '60px' }} />
           </Link>
         </div>
-        <div id="navMenu" className={NavbarStyles.navbarMenu}>
-          <span className="navbarBurger">a</span>
-          <span className="navbarBurger">b</span>
-          <span className="navbarBurger">c</span>
-          {/* <Link className={NavbarStyles.navbarItem} to="/writing">
+        <div id="navbarMenu" className={NavbarStyles.navbarMenu}>
+          <span className={NavbarStyles.navbarBurger}></span>
+          <span className={NavbarStyles.navbarBurger}></span>
+          <span className={NavbarStyles.navbarBurger}></span>
+        </div>
+
+        {/* <Link className={NavbarStyles.navbarItem} to="/writing">
             Writing
           </Link>
           <Link className={NavbarStyles.navbarItem} to="/work">
@@ -48,7 +49,6 @@ const Navbar = class extends React.Component {
           <Link className={NavbarStyles.navbarItem} to="/contact">
             Contact Me
           </Link> */}
-        </div>
       </nav>
     )
   }
