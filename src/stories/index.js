1e986ef189728558cda378940aed1d5bbeb96d64
Helen V. Holmes <helen.v.holmes@gmail.com>
2019-05-06 10:47:22 -0400

    Add the sample button code

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
+// import Navbar from '../components/Navbar';
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -8,4 +9,4 @@ storiesOf('Button', module)
   ))
   .add('with emoji', () => (
     <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ));   
+  ));
