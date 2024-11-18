3de293b17e0f3f6a2e7bbd8158faba84edf92bdb
Helen V. Holmes <1720093+scooooooooby@users.noreply.github.com>
2024-11-18 12:18:36 -0500

    How embarrassing

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
-// function init() {
-//   const lightEl = document.getElementById("light");
+function init() {
+  const lightEl = document.getElementById("light");
 
-//   // state
-//   let isLightVis = false;
-//   let lightX = 0;
-//   let lightY = 0;
-//   let prevSeconds;
+  // state
+  let isLightVis = false;
+  let lightX = 0;
+  let lightY = 0;
+  let prevSeconds;
 
-//   function fadeInLight() {
-//     isLightVis = true;
-//   }
-//   function fadeOutLight() {
-//     isLightVis = false;
-//   }
-//   function moveLight(x, y) {
-//     lightX = x;
-//     lightY = y;
-//   }
+  function fadeInLight() {
+    isLightVis = true;
+  }
+  function fadeOutLight() {
+    isLightVis = false;
+  }
+  function moveLight(x, y) {
+    lightX = x;
+    lightY = y;
+  }
 
-//   // render
-//   const render = () => {
-//     // update light position
-//     if (isLightVis) {
-//       lightEl.style.transform = `translate3d(calc(-180% + ${lightX}px), calc(-110% + ${lightY}px), 0px)`;
-//       lightEl.style.opacity = 1;
-//     } else {
-//       lightEl.style.opacity = 0;
-//     }
+  // render
+  const render = () => {
+    // update light position
+    if (isLightVis) {
+      lightEl.style.transform = `translate3d(calc(-180% + ${lightX}px), calc(-110% + ${lightY}px), 0px)`;
+      lightEl.style.opacity = 1;
+    } else {
+      lightEl.style.opacity = 0;
+    }
 
-//     // re-render
-//     requestAnimationFrame(render);
-//   };
+    // re-render
+    requestAnimationFrame(render);
+  };
 
-//   // initial render
-//   requestAnimationFrame(render);
+  // initial render
+  requestAnimationFrame(render);
 
-//   // listen for mouse movement (controls light position)
-//   document.body.addEventListener("mousemove", (e) => {
-//     const { pageX, pageY } = e;
+  // listen for mouse movement (controls light position)
+  document.body.addEventListener("mousemove", (e) => {
+    const { pageX, pageY } = e;
 
-//     moveLight(pageX, pageY);
-//     fadeInLight();
-//   });
+    moveLight(pageX, pageY);
+    fadeInLight();
+  });
 
-//   const mouseTarget = document.getElementById("spotlight");
+  const mouseTarget = document.getElementById("spotlight");
 
-//   mouseTarget.addEventListener("mouseenter", (e) => {
-//     console.log(e);
-//   });
-//   mouseTarget.addEventListener("mousemove", (e) => {
-//     console.log("move", e);
-//   });
-//   mouseTarget.addEventListener("mouseleave", (e) => {
-//     console.log(e);
-//   });
+  mouseTarget.addEventListener("mouseenter", (e) => {
+    console.log(e);
+  });
+  mouseTarget.addEventListener("mousemove", (e) => {
+    console.log("move", e);
+  });
+  mouseTarget.addEventListener("mouseleave", (e) => {
+    console.log(e);
+  });
 
-//   document.body.addEventListener("touchmove", (e) => {
-//     const { changedTouches } = e;
-//     const { pageX, pageY } = changedTouches[0];
+  document.body.addEventListener("touchmove", (e) => {
+    const { changedTouches } = e;
+    const { pageX, pageY } = changedTouches[0];
 
-//     moveLight(pageX, pageY);
-//     fadeInLight();
-//   });
+    moveLight(pageX, pageY);
+    fadeInLight();
+  });
 
-//   // fade in light only when over (prevents light "jumping")
-//   document.body.addEventListener("mouseleave", fadeOutLight);
-//   document.body.addEventListener("touchend", fadeOutLight);
-// }
+  // fade in light only when over (prevents light "jumping")
+  document.body.addEventListener("mouseleave", fadeOutLight);
+  document.body.addEventListener("touchend", fadeOutLight);
+}
 
-// init();
+init();
 
 const logo = document.getElementById("logo");
 
