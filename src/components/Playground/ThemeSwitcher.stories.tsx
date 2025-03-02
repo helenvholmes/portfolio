00c00812789e1999d08d80b0ecb34ae2fbd4d45a
Helen V. Holmes <1720093+helenvholmes@users.noreply.github.com>
2025-03-01 21:31:13 -0500

    Upgrades the ThemeSwitcher

--- src/components/Playground/ThemeSwitcher.stories.tsx
+++ src/components/Playground/ThemeSwitcher.stories.tsx
@@ -1,9 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { ThemeProvider } from "next-themes";
 
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
 const meta = {
   component: ThemeSwitcher,
+  decorators: [
+    (Story) => (
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
   title: "Playground/ThemeSwitcher",
 } satisfies Meta<typeof ThemeSwitcher>;
 
--- src/components/Playground/ThemeSwitcher.tsx
+++ src/components/Playground/ThemeSwitcher.tsx
@@ -17,6 +17,8 @@ export const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
 
+  console.log(theme);
+
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
     setMounted(true);
